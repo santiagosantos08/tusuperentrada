7917c15355d74507decb2a38850b21547cec4790
santiago <santiagosantos08@gmail.com>
2024-06-08 22:58:08 -0300

    getters y setters de usuario

--- Usuario.java
+++ Usuario.java
@@ -1,2 +1,35 @@
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private int id; //en caso del Comprador va a ser DNI, en Org va a ser CUIT/CUIL
+
+    Usuario(String nombre, String apellido, int id) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
