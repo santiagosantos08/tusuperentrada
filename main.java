8788cf1491ddfe311443b83e195d455a7f980854
santiago <santiagosantos08@gmail.com>
2024-06-09 22:13:44 -0300

    Merge de TU-29 registrar comprador a dev para luego pasar estos cambios al resto de branches que dependen del sistema de registro de usuarios

--- main.java
+++ main.java
@@ -1,26 +1,5 @@
 public class main {
     public static void main(String[] args){
-        //pruebas ===================================
-        // autenticador para funcionanilidad de registrar
-        /*
-        Autenticador.levantarDatos(); //inicializacion para que cargue del csv a memoria
-        int juanPerezNoExiste = Autenticador.registroExitoso("Juan", "Perez", "76238734", 1,"JuancitoSuperPassword");
-        int juanPerezYaExiste = Autenticador.registroExitoso("Juan", "Perez", "76238734", 1,"JuancitoSuperPassword");
-        int nombreInvalido = Autenticador.registroExitoso("J,uan", "Perez", "76238734", 1,"JuancitoSuperPassword");
-        int apellidoInvalido = Autenticador.registroExitoso("Juan", "P,erez", "76238734", 1,"JuancitoSuperPassword");
-        int malLongDNI = Autenticador.registroExitoso("Juan", "Perez", "78734", 1,"JuancitoSuperPassword");
-        int mismoNombreDistRol = Autenticador.registroExitoso("Juan", "Perez","45567856897", 2,"JuancitoSuperPassword");
-        int malLongCUIT = Autenticador.registroExitoso("Juan", "Perez","46897", 2,"JuancitoSuperPassword");
-        int argInvalido = Autenticador.registroExitoso("Juan", "Perez","46897", 77,"JuancitoSuperPassword");
-        System.out.println(juanPerezNoExiste);
-        System.out.println(juanPerezYaExiste);
-        System.out.println(nombreInvalido);
-        System.out.println(apellidoInvalido);
-        System.out.println(malLongDNI);
-        System.out.println(mismoNombreDistRol);
-        System.out.println(malLongCUIT);
-        System.out.println(argInvalido);
-        Autenticador.guardarDatos();
-        */
+
     }
 }
