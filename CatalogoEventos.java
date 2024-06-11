1f8f33703a8366c06fd8f061e674506fb28a3224
lisandroduhalde <lisandro.duhalde@gmail.com>
2024-06-11 20:06:29 -0300

    Agregué clase catalago de eventos

---
+++ CatalogoEventos.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CatalogoEventos {
+    private ArrayList<Evento> eventos;
+
+    public CatalogoEventos(){
+        this.eventos = new ArrayList<>();
+    }
+
+    public agregarEvento(Evento evento){
+        eventos.add(evento);
+    }
+
+    //Esto tendria que retornar una copia para no romper el encapsulamiento
+    public ArrayList<Evento> retornarEventos(){
+        return new ArrayList<>(eventos);
+    }
+
+}
